2dfc447bdbd36f56dad26e9046242044c7330cec
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-01-17 12:17:15 +0100

    after

--- addons/patients_list/models/patients.py
+++ addons/patients_list/models/patients.py
@@ -34,6 +34,7 @@ class Patients(models.Model):
     appointment_ids = fields.One2many('appointment.record', 'patient', string='Appointments')
     contact_ids = fields.One2many('contact.record', 'patient', string='Contacts')
     medical_histiory = fields.Many2many('medical.history', string='Medical history')
+    quotation_ids = fields.One2many('quotations', 'patient', string='Quotations')
 
     @api.depends('birthday')
     def _compute_age(self):
