2438ad1ee5fdabb56c0fa3aeb3ba6604be1e27a6
YoucefDjellas10 <djellasyoucef10@gmail.com>
2026-01-17 12:01:31 +0100

    after

--- addons/patients_list/__manifest__.py
+++ addons/patients_list/__manifest__.py
@@ -3,7 +3,16 @@
     'description': "Patients list",
     'depends': ['base'],
     'data': [
-
+        'views/countries_views.xml',
+        'views/patients_views.xml',
+        'views/cities_views.xml',
+        'views/appointment_view.xml',
+        'views/contact_view.xml',
+        'views/medical_histiry_views.xml',
+        'views/quotations_views.xml',
+        'views/treatments_views.xml',
+        'views/quotation_treatment_viwes.xml',
+        'views/teeth_views.xml',
 ],
     'license': 'LGPL-3',
 }
